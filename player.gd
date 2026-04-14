d50116726add52042766fd90e12496498fef2328
Adam Barański <baranski2023@gmail.com>
2026-04-14 22:54:13 +0200

    Map, camera changes
    Played with the camera and tile set settings for it to suit the game

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var direction: Vector2 = Vector2(1,1)
-var speed: int = 400
+var speed: int = 300
 
 func _physics_process(_delta: float):
 	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
