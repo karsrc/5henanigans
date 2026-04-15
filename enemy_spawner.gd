f76556f97b6d6d7e0a2cf0d8d21607dcb32e9692
Adam Barański <baranski2023@gmail.com>
2026-04-15 14:08:23 +0200

    Enemy spawner Test Change #1
    For now the spawner doesn't work properly but I want to try 2 ideas and try out which one will work the best for the game

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -5,11 +5,25 @@ var tile_size = 16
 
 func _on_timer_timeout():
 	var new_enemy = enemy_scene.instantiate()
+	var spawn_pos = Vector2.ZERO
 	
-	var random_x = randf_range(-11 * tile_size, 26 * tile_size)
-	var random_y = randf_range(-13 * tile_size, 28 * tile_size)
+
+
+	var edge = randi() % 4
 	
-	new_enemy.global_position = Vector2(random_x, random_y)
+	match edge:
+		0: # gora
+			spawn_pos.x = randf_range(-3 * tile_size, 18 * tile_size)
+			spawn_pos.y = -3 * tile_size
+		1: #dol
+			spawn_pos.x = randf_range(-3 * tile_size, 18 * tile_size)
+			spawn_pos.y = 18 * tile_size 
+		2: #lewo
+			spawn_pos.x = -3 * tile_size
+			spawn_pos.y = randf_range(-3 * tile_size, 18 * tile_size)
+		3: #prawo
+			spawn_pos.x = 18 * tile_size
+			spawn_pos.y = randf_range(-3 * tile_size, 18 * tile_size)
+			
+	new_enemy.global_position = spawn_pos
 	get_tree().current_scene.add_child(new_enemy)
-	
-	
