383f375b81d8acf5f00601dad4c7edbde957447c
Adam Barański <baranski2023@gmail.com>
2026-05-02 21:58:36 +0200

    Main Menu Implementation #2
    Added a logo and deleted some stuff, also the game actually works now

--- main.gd
+++ main.gd
@@ -4,36 +4,52 @@ extends Node2D
 @onready var spawner = $Spawner
 @onready var gameplay_hud = $hud
 @onready var main_menu_layer = $MainMenuLayer
+
+@onready var play_button = $MainMenuLayer/CenterContainer/VBoxContainer/PlayButton
 @onready var char_sprite = $MainMenuLayer/CenterContainer/VBoxContainer/CharacterRow/SpriteSpacer/AnimatedSprite2D
 @onready var char_name_label = $MainMenuLayer/CenterContainer/VBoxContainer/CharNameLabel
-@onready var play_button = $MainMenuLayer/CenterContainer/VBoxContainer/PlayButton
+
+# Hook up the new clickable buttons
+@onready var left_btn = $MainMenuLayer/CenterContainer/VBoxContainer/CharacterRow/LeftArrowBtn
+@onready var right_btn = $MainMenuLayer/CenterContainer/VBoxContainer/CharacterRow/RightArrowBtn
 
 var roster = ["VESSEL", "RYOMEN"]
 var current_idx = 0
+var is_starting = false 
 
 func _ready():
+	# Ensure the mouse is visible and usable!
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
 	if is_instance_valid(player): 
 		player.process_mode = Node.PROCESS_MODE_DISABLED
 		player.hide()
-		
 	if is_instance_valid(spawner): spawner.process_mode = Node.PROCESS_MODE_DISABLED
 	if is_instance_valid(gameplay_hud): gameplay_hud.hide()
 	
-	play_button.focus_mode = Control.FOCUS_NONE
-	play_button.pressed.connect(_on_play_pressed)
+	# Connect the buttons for mouse clicks
+	if is_instance_valid(play_button):
+		play_button.pressed.connect(_on_play_pressed)
+	if is_instance_valid(left_btn):
+		left_btn.pressed.connect(func(): cycle_character(-1))
+	if is_instance_valid(right_btn):
+		right_btn.pressed.connect(func(): cycle_character(1))
+		
 	update_character_display()
 
 func _input(event):
-	if not main_menu_layer.visible: return
+	if not main_menu_layer.visible or is_starting: return
 	
+	# Keep keyboard support alive
 	if event.is_action_pressed("ui_left"):
 		cycle_character(-1)
 	elif event.is_action_pressed("ui_right"):
 		cycle_character(1)
+	elif event.is_action_pressed("ui_accept"):
+		_on_play_pressed()
 
 func cycle_character(direction: int):
 	current_idx += direction
-	
 	if current_idx < 0: current_idx = roster.size() - 1
 	elif current_idx >= roster.size(): current_idx = 0
 		
@@ -41,22 +57,27 @@ func cycle_character(direction: int):
 	
 	if is_instance_valid(char_sprite):
 		var tw = create_tween()
-		char_sprite.modulate = Color(10, 10, 10, 1)
+		char_sprite.modulate = Color(10, 10, 10, 1) # Flash white
 		tw.tween_property(char_sprite, "modulate", Color(1, 1, 1, 1), 0.15)
 
 func update_character_display():
 	char_name_label.text = roster[current_idx]
 	if roster[current_idx] == "RYOMEN":
-		char_sprite.self_modulate = Color(1.5, 0.5, 0.5)
+		char_sprite.self_modulate = Color("#df84a5") # Your palette pink
 	else:
 		char_sprite.self_modulate = Color(1, 1, 1)
 
 func _on_play_pressed():
-	play_button.disabled = true
+	if is_starting: return
+	is_starting = true
+	
 	var fade_tween = create_tween()
-	fade_tween.tween_property(main_menu_layer, "modulate:a", 0.0, 0.5)
+	fade_tween.tween_property(main_menu_layer, "modulate:a", 0.0, 0.8)
+	
 	fade_tween.tween_callback(func():
 		main_menu_layer.hide()
+		
+		# UNFREEZE THE GAME
 		if is_instance_valid(player): 
 			player.process_mode = Node.PROCESS_MODE_INHERIT
 			player.show()
--- main_menu.gd
+++ main_menu.gd
@@ -1,55 +1,76 @@
 extends Node2D
 
-@onready var char_sprite = $UILayer/CenterContainer/VBoxContainer/CharacterRow/CharSprite
-@onready var char_name_label = $UILayer/CenterContainer/VBoxContainer/CharNameLabel
-@onready var play_button = $UILayer/CenterContainer/VBoxContainer/PlayButton
-@onready var crt_shader = $ShaderLayer/ColorRect # Make sure this path is correct!
+@onready var player = $Player
+@onready var spawner = $Spawner
+@onready var gameplay_hud = $hud
+@onready var main_menu_layer = $MainMenuLayer
+@onready var play_button = $MainMenuLayer/CenterContainer/VBoxContainer/PlayButton
+@onready var char_sprite = $MainMenuLayer/CenterContainer/VBoxContainer/CharacterRow/SpriteSpacer/AnimatedSprite2D
+@onready var char_name_label = $MainMenuLayer/CenterContainer/VBoxContainer/CharNameLabel
+@onready var left_btn = $MainMenuLayer/CenterContainer/VBoxContainer/CharacterRow/LeftArrowBtn
+@onready var right_btn = $MainMenuLayer/CenterContainer/VBoxContainer/CharacterRow/RightArrowBtn
 
-# Add future characters here
-var roster = [
-	{"name": "VESSEL", "anim": "right"},
-	{"name": "KING OF CURSES", "anim": "right"}  
-]
+var roster = ["VESSEL", "RYOMEN"]
 var current_idx = 0
+var is_starting = false 
 
 func _ready():
-	play_button.focus_mode = Control.FOCUS_NONE 
-	play_button.pressed.connect(_on_play_pressed)
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	if is_instance_valid(player): 
+		player.process_mode = Node.PROCESS_MODE_DISABLED
+		player.hide()
+	if is_instance_valid(spawner): spawner.process_mode = Node.PROCESS_MODE_DISABLED
+	if is_instance_valid(gameplay_hud): gameplay_hud.hide()
+	if is_instance_valid(play_button):
+		play_button.pressed.connect(_on_play_pressed)
+	if is_instance_valid(left_btn):
+		left_btn.pressed.connect(func(): cycle_character(-1))
+	if is_instance_valid(right_btn):
+		right_btn.pressed.connect(func(): cycle_character(1))
+		
 	update_character_display()
 
 func _input(event):
+	if not main_menu_layer.visible or is_starting: return
 	if event.is_action_pressed("ui_left"):
 		cycle_character(-1)
 	elif event.is_action_pressed("ui_right"):
 		cycle_character(1)
+	elif event.is_action_pressed("ui_accept"):
+		_on_play_pressed()
+
 func cycle_character(direction: int):
 	current_idx += direction
-	if current_idx < 0:
-		current_idx = roster.size() - 1
-	elif current_idx >= roster.size():
-		current_idx = 0
+	if current_idx < 0: current_idx = roster.size() - 1
+	elif current_idx >= roster.size(): current_idx = 0
+		
 	update_character_display()
-	var flash_tween = create_tween()
-	char_sprite.modulate = Color(10, 10, 10, 1)
-	flash_tween.tween_property(char_sprite, "modulate", Color(1, 1, 1, 1), 0.15)
+	
+	if is_instance_valid(char_sprite):
+		var tw = create_tween()
+		char_sprite.modulate = Color(10, 10, 10, 1)
+		tw.tween_property(char_sprite, "modulate", Color(1, 1, 1, 1), 0.15)
 
 func update_character_display():
-	var char_data = roster[current_idx]
-	char_name_label.text = char_data["name"]
-	if char_sprite.has_method("play"):
-		char_sprite.play(char_data["anim"])
+	char_name_label.text = roster[current_idx]
+	if roster[current_idx] == "RYOMEN":
+		char_sprite.self_modulate = Color("#df84a5")
+	else:
+		char_sprite.self_modulate = Color(1, 1, 1)
 
 func _on_play_pressed():
-	play_button.disabled = true
-	if crt_shader and crt_shader.material:
-		var mat = crt_shader.material
-		var glitch_tween = create_tween()
-		glitch_tween.tween_method(func(v): mat.set_shader_parameter("rgb_shift", v), 0.002, 0.05, 0.1)
-		glitch_tween.tween_method(func(v): mat.set_shader_parameter("scanline_intensity", v), 0.35, 1.0, 0.1)
-		glitch_tween.tween_method(func(v): mat.set_shader_parameter("rgb_shift", v), 0.05, 0.002, 0.1)
-		glitch_tween.tween_callback(load_actual_game).set_delay(0.2)
-	else:
-		load_actual_game()
-
-func load_actual_game():
-	get_tree().change_scene_to_file("res://main.tscn")
+	if is_starting: return
+	is_starting = true
+	
+	var fade_tween = create_tween()
+	fade_tween.tween_property(main_menu_layer, "modulate:a", 0.0, 0.8)
+	
+	fade_tween.tween_callback(func():
+		main_menu_layer.hide()
+		if is_instance_valid(player): 
+			player.process_mode = Node.PROCESS_MODE_INHERIT
+			player.show()
+		if is_instance_valid(spawner): spawner.process_mode = Node.PROCESS_MODE_INHERIT
+		if is_instance_valid(gameplay_hud): gameplay_hud.show()
+	)
